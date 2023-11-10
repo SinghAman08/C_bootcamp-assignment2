92e4c1d31d6004bcc8b7863412988eb0e3fe7e7e
Aman <amansingh21@gmail.com>
2023-11-10 17:34:21 +0530

    v2.0

---
+++ question7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char character;
+    printf("enter a character\n");
+    scanf("%c",&character);
+
+    printf("ASCII code of %c is %d\n",character,character);    
+    return 0;
+}
